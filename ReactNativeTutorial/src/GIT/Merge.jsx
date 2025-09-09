b12a920bdfd5e60638547226edbe39f4b2b4ce2e
Zainab Iman <zainabiman541@gmail.com>
2025-09-08 21:41:22 -0700

    merge

---
+++ ReactNativeTutorial/src/GIT/Merge.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Merge = () => {
+  return (
+    <View>
+      <Text>Merge</Text>
+    </View>
+  )
+}
+
+export default Merge
